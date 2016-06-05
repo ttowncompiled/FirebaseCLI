9e41ed4484409ed402aa5052b86c1e1a700eb2db
Ian Riley <ian.riley.915@gmail.com>
2016-06-04 20:27:17 -0500

    Adds push command.

--- firebase-cli.js
+++ firebase-cli.js
@@ -1,42 +1,68 @@
 var firebase = require('firebase');
 var fs = require('fs');
 
+var COMMAND = 'command';
+var CONFIG = 'config';
+var DATA = 'data';
+var EQUAL = '=';
+var HELP = '-h';
+var PUSH = 'push';
+var REF = 'ref';
+var UTF8 = 'utf8';
+
 function preprocess() {
   var args = process.argv.slice(2);
-  if (args[0] == '-h') {
-    console.log('no commands currently');
+  if (args[0] == HELP) {
+    console.log('commands:');
+    console.log('\tpush ref=<path-from-root-of-firebase> data=<path-to-data>');
     process.exit();
   }
   if (args.length < 2) {
-    console.log('usage: node firebase-cli.js config=<path-to-config> <command>');
-    console.log('for a list of commands: node firebase-cli.js -h');
+    console.error('usage: node firebase-cli.js config=<path-to-config> <command>');
+    console.error('for a list of commands: node firebase-cli.js -h');
     process.exit(1);
   }
   var params = {};
   args.forEach(function(arg) {
-    if (arg.includes('=')) {
-      var parts = arg.split('=');
+    if (arg.includes(EQUAL)) {
+      var parts = arg.split(EQUAL);
       params[parts[0]] = parts[1];
-    } else if (!('command' in params)) {
-      params['command'] = arg;
+    } else if (!(COMMAND in params)) {
+      params[COMMAND] = arg;
     } else {
-      console.log('ERROR: can not handle more than one command');
+      console.error('ERROR: can not handle more than one command');
       process.exit(1);
     }
   });
-  if (!('config' in params)) {
-    console.log('ERROR: Firebase configuration file must be provided');
+  if (!(CONFIG in params)) {
+    console.error('ERROR: Firebase configuration file must be provided');
     process.exit(1);
   }
-  if (!('command' in params)) {
-    console.log('for a list of commands: node firebase-cli.js -h');
+  if (!(COMMAND in params)) {
+    console.error('for a list of commands: node firebase-cli.js -h');
     process.exit(1);
   }
   return params;
 }
 
+function configureFirebase(configFilePath) {
+  var config = JSON.parse(fs.readFileSync(configFilePath, UTF8));
+  return firebase.initializeApp(config);
+}
+
+function push(fb, ref, dataFilePath) {
+  var data = JSON.parse(fs.readFileSync(dataFilePath, UTF8));
+  var db = fb.database().ref(ref);
+  data.forEach(function(item) {
+    db.push(item);
+  });
+}
+
 function main(params) {
-  console.log(params);
+  var fb = configureFirebase(params[CONFIG]);
+  if (params[COMMAND] == PUSH) {
+    push(fb, params[REF], params[DATA]);
+  }
 }
 
 main(preprocess());
